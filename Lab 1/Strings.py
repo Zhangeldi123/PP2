d26b934992e198a0210bc709c1819d5117812b04
Zhangeldi <zhanikshakimurat@gmail.com>
2024-01-24 10:20:24 +0600

    Lab 1

---
+++ Lab 1/Strings.py
@@ -0,0 +1,17 @@
+x = "Hello World"
+print(len(x))
+txt = "Hello World"
+y = txt[0]
+text = "Hello World"
+z = text[2:5]
+Text = "Hello World"
+w = Text.strip()
+TExt = "Hello World"
+TExt = TExt.upper()
+TEXt = "Hello World"
+TEXt = TEXt.lower()
+TEXT = "Hello World"
+TEXT = TEXT.replace("H", "J")
+age = 36
+TeXT = "My name is John, and I am {}"
+print(TeXT.format(age))
